548e829abfdb32cf6a2d26325b58a802ff8e1239
FIRAT DOĞAN <firatdogan0@gmail.com>
2020-01-21 18:50:32 +0200

    Initial commit

---
+++ WindowsFormsApp56/Panel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp56
+{
+    public partial class Panel : Form
+    {
+        public Panel()
+        {
+            InitializeComponent();
+        }
+
+        private void Panel_Load(object sender, EventArgs e)
+        {
+            Form1 frm1 = new Form1();
+            label1.Text = "Hoşgeldiniz "+ frm1.tanimlananid;
+        }
+    }
+}
